f6a2c5e06319c717b9f1e6406ba0ae2c179e775a
shubham kumar <sk0228389@gmail.com>
2024-07-20 16:20:19 +0530

    first commit

---
+++ src/app/MyComponents/table/table.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-table',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css']
+})
+export class TableComponent {
+  userData = [
+    {
+      id: 1,
+      name: 'John Doe',
+      workouts: [
+        { type: 'Running', minutes: 30 },
+        { type: 'Cycling', minutes: 45 }
+      ]
+    },
+    {
+      id: 2,
+      name: 'Jane Smith',
+      workouts: [
+        { type: 'Swimming', minutes: 60 },
+        { type: 'Running', minutes: 20 }
+      ]
+    },
+    {
+      id: 3,
+      name: 'Mike Johnson',
+      workouts: [
+        { type: 'Yoga', minutes: 50 },
+        { type: 'Cycling', minutes: 40 }
+      ]
+    }
+  ];
+
+  // Method to calculate total workout minutes
+  getTotalWorkoutMinutes(workouts: { type: string, minutes: number }[]): number {
+    return workouts.reduce((total, workout) => total + workout.minutes, 0);
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './MyComponents/home/home.component';
+import { TableComponent } from './MyComponents/table/table.component';
+import { NavbarComponent } from './MyComponents/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HomeComponent,TableComponent,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
